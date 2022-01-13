db8083fa1747d6fe0a78cae6713ac408532b1da4
jdesgarennes <johndesgarennes@gmail.com>
2022-01-13 03:19:12 -0500

    Created base react environment

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Header from './components/Header';
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <h1>React-portfolio</h1>
+    </div>
+  );
+}
+
+export default App;
